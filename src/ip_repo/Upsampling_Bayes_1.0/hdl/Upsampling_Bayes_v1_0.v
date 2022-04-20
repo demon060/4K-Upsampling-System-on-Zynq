8c49db1c11d755f4f6e2f6ec06edf402a3ee4eb0
Megumism <philomegumi@gmail.com>
2022-04-20 21:41:34 +0800

    IP更新了接口转换

--- src/ip_repo/Upsampling_Bayes_1.0/hdl/Upsampling_Bayes_v1_0.v
+++ src/ip_repo/Upsampling_Bayes_1.0/hdl/Upsampling_Bayes_v1_0.v
@@ -4,7 +4,7 @@
 	module Upsampling_Bayes_v1_0 #
 	(
 		// Users to add parameters here
-
+		parameter PIXEL_WIDTH = 24,
 		// User parameters ends
 		// Do not modify the parameters beyond this line
 
@@ -18,7 +18,8 @@
 	)
 	(
 		// Users to add ports here
-
+		input wire clk,
+		input wire rst_n,
 		// User ports ends
 		// Do not modify the ports beyond this line
 
@@ -41,6 +42,24 @@
 		output wire  m00_axis_tlast,
 		input wire  m00_axis_tready
 	);
+
+	// axis buffer输出数据
+	wire [C_S00_AXIS_TDATA_WIDTH-1:0] s_axis_get_data;
+	// Super_Res模块在忙，不收新像素
+	wire sr_busy;
+	// axis转pixel buffer已满
+	wire axis_pixel_buffer_full;
+	// axis buffer为空
+	wire s_axis_empty;
+	// Super_Res输入像素有效
+	wire sr_pin_en;
+	// Super_Res输出像素有效
+	wire sr_pout_en;
+	// Super_Res的输入像素
+	wire [PIXEL_WIDTH-1:0] pixel_low;
+	// Super_Res的输出像素
+	wire [PIXEL_WIDTH*4-1:0] pixel_high;
+
 // Instantiation of Axi Bus Interface S00_AXIS
 	Upsampling_Bayes_v1_0_S00_AXIS # ( 
 		.C_S_AXIS_TDATA_WIDTH(C_S00_AXIS_TDATA_WIDTH)
@@ -51,13 +70,17 @@
 		.S_AXIS_TDATA(s00_axis_tdata),
 		.S_AXIS_TSTRB(s00_axis_tstrb),
 		.S_AXIS_TLAST(s00_axis_tlast),
-		.S_AXIS_TVALID(s00_axis_tvalid)
+		.S_AXIS_TVALID(s00_axis_tvalid),
+		.S_AXIS_get_data(s_axis_get_data),
+		.fifo_rden(!axis_pixel_buffer_full),
+		.fifo_empty_flag(s_axis_empty)
 	);
 
 // Instantiation of Axi Bus Interface M00_AXIS
 	Upsampling_Bayes_v1_0_M00_AXIS # ( 
 		.C_M_AXIS_TDATA_WIDTH(C_M00_AXIS_TDATA_WIDTH),
-		.C_M_START_COUNT(C_M00_AXIS_START_COUNT)
+		.C_M_START_COUNT(C_M00_AXIS_START_COUNT),
+		.PIXEL_WIDTH(PIXEL_WIDTH)
 	) Upsampling_Bayes_v1_0_M00_AXIS_inst (
 		.M_AXIS_ACLK(m00_axis_aclk),
 		.M_AXIS_ARESETN(m00_axis_aresetn),
@@ -65,11 +88,40 @@
 		.M_AXIS_TDATA(m00_axis_tdata),
 		.M_AXIS_TSTRB(m00_axis_tstrb),
 		.M_AXIS_TLAST(m00_axis_tlast),
-		.M_AXIS_TREADY(m00_axis_tready)
+		.M_AXIS_TREADY(m00_axis_tready),
+		.M_AXIS_send_data(pixel_high),
+        .fifo_wren(sr_pout_en),
+        .fifo_full(m_axis_full)
 	);
 
 	// Add user logic here
 
+	AXIS_to_pixel_buffer #(
+ 		.AXIS_TDATA_WIDTH(C_S00_AXIS_TDATA_WIDTH),
+		.PIXEL_WIDTH(PIXEL_WIDTH)
+	) AXIS_to_pixel_buffer_0 (
+		.clk(clk),
+		.rst_n(rst_n),
+		.data_in(S_AXIS_get_data),
+		.pixel_out(pixel_low),
+		.buf_full(axis_pixel_buffer_full),
+		.trans_eff(sr_pin_en),
+		.buf_rden(!sr_busy),
+		.buf_wren(!s_axis_empty)
+	);
+
+	Super_Res #(
+		.PIXEL_WIDTH(PIXEL_WIDTH)
+	) Super_Res_0 (
+		.clk(clk),
+		.rst_n(rst_n),
+		.pixel_in(pixel_low),
+		.pin_en(sr_pin_en),
+		.pixel_out(pixel_high),
+		.pout_en(sr_pout_en),
+		.busy(sr_busy),
+		.stuck(m_axis_full)
+	);
 	// User logic ends
 
 	endmodule
--- src/ip_repo/Upsampling_Bayes_1.0/hdl/Upsampling_Bayes_v1_0_S00_AXIS.v
+++ src/ip_repo/Upsampling_Bayes_1.0/hdl/Upsampling_Bayes_v1_0_S00_AXIS.v
@@ -1,167 +1,190 @@
 
 `timescale 1 ns / 1 ps
 
-	module Upsampling_Bayes_v1_0_S00_AXIS #
-	(
-		// Users to add parameters here
+module Upsampling_Bayes_v1_0_S00_AXIS #
+       (
+           // Users to add parameters here
 
-		// User parameters ends
-		// Do not modify the parameters beyond this line
+           // User parameters ends
+           // Do not modify the parameters beyond this line
 
-		// AXI4Stream sink: Data Width
-		parameter integer C_S_AXIS_TDATA_WIDTH	= 32
-	)
-	(
-		// Users to add ports here
+           // AXI4Stream sink: Data Width
+           parameter integer C_S_AXIS_TDATA_WIDTH	= 32
+       )
+       (
+           // Users to add ports here
+           output wire [C_S_AXIS_TDATA_WIDTH-1:0] S_AXIS_get_data,
+           input wire fifo_rden,
+           output wire fifo_empty_flag,
 
-		// User ports ends
-		// Do not modify the ports beyond this line
+           // User ports ends
+           // Do not modify the ports beyond this line
 
-		// AXI4Stream sink: Clock
-		input wire  S_AXIS_ACLK,
-		// AXI4Stream sink: Reset
-		input wire  S_AXIS_ARESETN,
-		// Ready to accept data in
-		output wire  S_AXIS_TREADY,
-		// Data in
-		input wire [C_S_AXIS_TDATA_WIDTH-1 : 0] S_AXIS_TDATA,
-		// Byte qualifier
-		input wire [(C_S_AXIS_TDATA_WIDTH/8)-1 : 0] S_AXIS_TSTRB,
-		// Indicates boundary of last packet
-		input wire  S_AXIS_TLAST,
-		// Data is in valid
-		input wire  S_AXIS_TVALID
-	);
-	// function called clogb2 that returns an integer which has the 
-	// value of the ceiling of the log base 2.
-	function integer clogb2 (input integer bit_depth);
-	  begin
-	    for(clogb2=0; bit_depth>0; clogb2=clogb2+1)
-	      bit_depth = bit_depth >> 1;
-	  end
-	endfunction
+           // AXI4Stream sink: Clock
+           input wire  S_AXIS_ACLK,
+           // AXI4Stream sink: Reset
+           input wire  S_AXIS_ARESETN,
+           // Ready to accept data in
+           output wire  S_AXIS_TREADY,
+           // Data in
+           input wire [C_S_AXIS_TDATA_WIDTH-1 : 0] S_AXIS_TDATA,
+           // Byte qualifier
+           input wire [(C_S_AXIS_TDATA_WIDTH/8)-1 : 0] S_AXIS_TSTRB,
+           // Indicates boundary of last packet
+           input wire  S_AXIS_TLAST,
+           // Data is in valid
+           input wire  S_AXIS_TVALID
+       );
+// function called clogb2 that returns an integer which has the
+// value of the ceiling of the log base 2.
+function integer clogb2 (input integer bit_depth);
+    begin
+        for(clogb2=0; bit_depth>0; clogb2=clogb2+1)
+            bit_depth = bit_depth >> 1;
+    end
+endfunction
 
-	// Total number of input data.
-	localparam NUMBER_OF_INPUT_WORDS  = 8;
-	// bit_num gives the minimum number of bits needed to address 'NUMBER_OF_INPUT_WORDS' size of FIFO.
-	localparam bit_num  = clogb2(NUMBER_OF_INPUT_WORDS-1);
-	// Define the states of state machine
-	// The control state machine oversees the writing of input streaming data to the FIFO,
-	// and outputs the streaming data from the FIFO
-	parameter [1:0] IDLE = 1'b0,        // This is the initial/idle state 
+// Total number of input data.
+localparam NUMBER_OF_INPUT_WORDS  = 8;
+// bit_num gives the minimum number of bits needed to address 'NUMBER_OF_INPUT_WORDS' size of FIFO.
+localparam bit_num  = clogb2(NUMBER_OF_INPUT_WORDS-1);//4
+// Define the states of state machine
+// The control state machine oversees the writing of input streaming data to the FIFO,
+// and outputs the streaming data from the FIFO
+parameter [1:0] IDLE = 1'b0,        // This is the initial/idle state
 
-	                WRITE_FIFO  = 1'b1; // In this state FIFO is written with the
-	                                    // input stream data S_AXIS_TDATA 
-	wire  	axis_tready;
-	// State variable
-	reg mst_exec_state;  
-	// FIFO implementation signals
-	genvar byte_index;     
-	// FIFO write enable
-	wire fifo_wren;
-	// FIFO full flag
-	reg fifo_full_flag;
-	// FIFO write pointer
-	reg [bit_num-1:0] write_pointer;
-	// sink has accepted all the streaming data and stored in FIFO
-	  reg writes_done;
-	// I/O Connections assignments
+          WRITE_FIFO  = 1'b1; // In this state FIFO is written with the
+// input stream data S_AXIS_TDATA
+wire  	axis_tready;
+// State variable
+reg mst_exec_state;
+// FIFO implementation signals
+genvar byte_index;
+// FIFO full flag
+wire fifo_full_flag;
+// FIFO write enable
+wire fifo_wren;
+// FIFO read enable
+// wire fifo_rden;
+// FIFO write pointer
+reg [bit_num:0] write_pointer;
+// FIFO read pointer
+reg [bit_num:0] read_pointer;
+// sink has accepted all the streaming data and stored in FIFO
+reg writes_done;
 
-	assign S_AXIS_TREADY	= axis_tready;
-	// Control state machine implementation
-	always @(posedge S_AXIS_ACLK) 
-	begin  
-	  if (!S_AXIS_ARESETN) 
-	  // Synchronous reset (active low)
-	    begin
-	      mst_exec_state <= IDLE;
-	    end  
-	  else
-	    case (mst_exec_state)
-	      IDLE: 
-	        // The sink starts accepting tdata when 
-	        // there tvalid is asserted to mark the
-	        // presence of valid streaming data 
-	          if (S_AXIS_TVALID)
-	            begin
-	              mst_exec_state <= WRITE_FIFO;
-	            end
-	          else
-	            begin
-	              mst_exec_state <= IDLE;
-	            end
-	      WRITE_FIFO: 
-	        // When the sink has accepted all the streaming input data,
-	        // the interface swiches functionality to a streaming master
-	        if (writes_done)
-	          begin
-	            mst_exec_state <= IDLE;
-	          end
-	        else
-	          begin
-	            // The sink accepts and stores tdata 
-	            // into FIFO
-	            mst_exec_state <= WRITE_FIFO;
-	          end
+// I/O Connections assignments
+assign S_AXIS_TREADY	= axis_tready;
 
-	    endcase
-	end
-	// AXI Streaming Sink 
-	// 
-	// The example design sink is always ready to accept the S_AXIS_TDATA  until
-	// the FIFO is not filled with NUMBER_OF_INPUT_WORDS number of input words.
-	assign axis_tready = ((mst_exec_state == WRITE_FIFO) && (write_pointer <= NUMBER_OF_INPUT_WORDS-1));
+// Control state machine implementation
+always @(posedge S_AXIS_ACLK)
+begin
+    if (!S_AXIS_ARESETN)
+        // Synchronous reset (active low)
+    begin
+        mst_exec_state <= IDLE;
+    end
+    else
+    case (mst_exec_state)
+        IDLE:
+            // The sink starts accepting tdata when
+            // there tvalid is asserted to mark the
+            // presence of valid streaming data
+            if (S_AXIS_TVALID)
+            begin
+                mst_exec_state <= WRITE_FIFO;
+            end
+            else
+            begin
+                mst_exec_state <= IDLE;
+            end
+        WRITE_FIFO:
+            // When the sink has accepted all the streaming input data,
+            // the interface swiches functionality to a streaming master
+            if (writes_done)
+            begin
+                mst_exec_state <= IDLE;
+            end
+            else
+            begin
+                // The sink accepts and stores tdata
+                // into FIFO
+                mst_exec_state <= WRITE_FIFO;
+            end
 
-	always@(posedge S_AXIS_ACLK)
-	begin
-	  if(!S_AXIS_ARESETN)
-	    begin
-	      write_pointer <= 0;
-	      writes_done <= 1'b0;
-	    end  
-	  else
-	    if (write_pointer <= NUMBER_OF_INPUT_WORDS-1)
-	      begin
-	        if (fifo_wren)
-	          begin
-	            // write pointer is incremented after every write to the FIFO
-	            // when FIFO write signal is enabled.
-	            write_pointer <= write_pointer + 1;
-	            writes_done <= 1'b0;
-	          end
-	          if ((write_pointer == NUMBER_OF_INPUT_WORDS-1)|| S_AXIS_TLAST)
-	            begin
-	              // reads_done is asserted when NUMBER_OF_INPUT_WORDS numbers of streaming data 
-	              // has been written to the FIFO which is also marked by S_AXIS_TLAST(kept for optional usage).
-	              writes_done <= 1'b1;
-	            end
-	      end  
-	end
+    endcase
+end
+// AXI Streaming Sink
+//
+// The example design sink is always ready to accept the S_AXIS_TDATA  until
+// the FIFO is not filled with NUMBER_OF_INPUT_WORDS number of input words.
+assign axis_tready = ((mst_exec_state == WRITE_FIFO) && (!fifo_full_flag));
 
-	// FIFO write enable generation
-	assign fifo_wren = S_AXIS_TVALID && axis_tready;
+always@(posedge S_AXIS_ACLK)
+begin
+    if(!S_AXIS_ARESETN)
+    begin
+        write_pointer <= 0;
+        writes_done <= 1'b0;
+    end
+    else begin
+        if (fifo_wren)
+        begin
+            // write pointer is incremented after every write to the FIFO
+            // when FIFO write signal is enabled.
+            write_pointer <= write_pointer + 1;
+            writes_done <= 1'b0;
+        end
+        if (!fifo_wren || S_AXIS_TLAST)
+        begin
+            // reads_done is asserted when NUMBER_OF_INPUT_WORDS numbers of streaming data
+            // has been written to the FIFO which is also marked by S_AXIS_TLAST(kept for optional usage).
+            if (!fifo_full_flag) begin
+                write_pointer <= write_pointer + 1;
+            end
+            writes_done <= 1'b1;
+        end
+    end
+end
 
-	// FIFO Implementation
-	generate 
-	  for(byte_index=0; byte_index<= (C_S_AXIS_TDATA_WIDTH/8-1); byte_index=byte_index+1)
-	  begin:FIFO_GEN
+// FIFO write enable generation
+// fifo_wren为高的时候，才会允许读入改变write_pointer
+// -> pointer满了之后，write_done被拉高
+// -> write_done被拉高，状态机进入IDLE
+// -> 但问题不大，只要主机还是S_AXIS_TVALID，状态机下一个周期还会进WRITE_FIFO
+// -> pointer如果还是满的，那什么也不发生
+assign fifo_wren = S_AXIS_TVALID && axis_tready;
 
-	    reg  [(C_S_AXIS_TDATA_WIDTH/4)-1:0] stream_data_fifo [0 : NUMBER_OF_INPUT_WORDS-1];
+// FIFO Implementation
+reg  [C_S_AXIS_TDATA_WIDTH-1:0] stream_data_fifo [0 : NUMBER_OF_INPUT_WORDS-1];
+// Streaming input data is stored in FIFO
+always @( posedge S_AXIS_ACLK )
+begin
+    if (fifo_wren)// && S_AXIS_TSTRB[byte_index])
+    begin
+        //这是一个byte的fifo，具体几个byte由generate定义
+        stream_data_fifo[write_pointer] <= S_AXIS_TDATA;
+    end
+end
 
-	    // Streaming input data is stored in FIFO
+assign S_AXIS_get_data = stream_data_fifo[read_pointer];
 
-	    always @( posedge S_AXIS_ACLK )
-	    begin
-	      if (fifo_wren)// && S_AXIS_TSTRB[byte_index])
-	        begin
-	          stream_data_fifo[write_pointer] <= S_AXIS_TDATA[(byte_index*8+7) -: 8];
-	        end  
-	    end  
-	  end		
-	endgenerate
+// Add user logic here
+// 定义fifo的空满
+assign fifo_full_flag = (write_pointer - read_pointer == {1'b1,{(bit_num){1'b0}}}) || !S_AXIS_ARESETN;
+assign fifo_empty_flag = write_pointer == read_pointer;
 
-	// Add user logic here
+always @(posedge S_AXIS_ACLK) begin
+    if (!S_AXIS_ARESETN) begin
+        read_pointer <= 0;
+    end
+    else begin
+        if (!fifo_empty_flag && fifo_rden) begin
+            read_pointer <= read_pointer + 1;
+        end
+    end
+end
 
-	// User logic ends
+// User logic ends
 
-	endmodule
+endmodule
